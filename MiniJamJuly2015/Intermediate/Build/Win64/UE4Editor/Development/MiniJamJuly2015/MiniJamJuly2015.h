a615e73dbb913a370fee86638d208c67bdd4dbfc
Dennis Lieu <dlieu@lieudennis.com>
2015-07-11 15:34:24 +0200

    - Delete Intermediate and Saved folder

--- MiniJamJuly2015/Intermediate/Build/Win64/UE4Editor/Development/MiniJamJuly2015/MiniJamJuly2015.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_MINIJAMJULY2015_H__
-#define __AUTO_MINIJAMJULY2015_H__
-//Last Write: 11-Jul-15 11:40:22 +00:00
-#include "C:\Users\Rezan\Documents\GitHub\BerlinMiniJamJuly2015\MiniJamJuly2015\Source\MiniJamJuly2015\MiniJamJuly2015.h"
-#endif//__AUTO_MINIJAMJULY2015_H__
--- MiniJamJuly2015/Intermediate/Build/Win64/UE4Editor/Development/MiniJamJuly2015/PCH.MiniJamJuly2015.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:\Users\Rezan\Documents\GitHub\BerlinMiniJamJuly2015\MiniJamJuly2015\Source\MiniJamJuly2015\MiniJamJuly2015.h"
